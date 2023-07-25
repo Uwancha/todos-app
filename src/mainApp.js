866f4eeb53c2baed156d47c330fac149095f845b
Uwancha <sito2alemu@gmail.com>
2023-07-25 19:27:48 +0300

    refac: refactor default todo creating logic to improve maintainability

--- src/mainApp.js
+++ src/mainApp.js
@@ -2,19 +2,19 @@ import "./style.css"
 import { renderDefaultTasks, renderProjectList, renderTodoList } from "./domModule";
 import { createToDoFactory, createProjectFactory } from "./appLogic";
 
-const home = []
+const home = createProjectFactory("home")
 const one = createToDoFactory("Coding", "Do coding", "everyday", "high");
   const two = createToDoFactory("Reading", "Read 10 pages", "everyday", "high");
   const three = createToDoFactory("Exercise", "Do exercises", "every day", "high");
   const four = createToDoFactory("Meditate", "Meditating", "everyday", "high");
 
-home.push(one);
-home.push(two);
-home.push(three);
-home.push(four);
+home.addTodo(one);
+home.addTodo(two);
+home.addTodo(three);
+home.addTodo(four);
 one.markDone()
 
-renderDefaultTasks(home);
+renderDefaultTasks(home.todos);
 
 
 
